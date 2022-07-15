da927551154c4a3d91ae162ad9f7b59cf7145ea5
ariaxhera <velviit17@gmail.com>
2022-07-15 21:31:57 +0200

    Eexercise: Level 1

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,27 @@
+// ##### Exercise: Level 1
+
+//1.
+const empty = Array()
+//2.
+const arr = Array(1,2,3,4,5,6,7)
+//3.
+console.log(arr.length)
+//4.
+console.log(arr[0])
+console.log(arr[3])
+console.log(arr[arr.length - 1])
+//5.
+const mixedDataTypes = Array(1,2,'Three',4,5,'Six','Seven')
+console.log(arr.length)
+//6.
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+//7.
+console.log(itCompanies)
+//8.
+console.log(itCompanies.length)
+//9.
+console.log(itCompanies[0])
+console.log(itCompanies[itCompanies.length - 1])
+console.log(itCompanies[3])
+//10.
+

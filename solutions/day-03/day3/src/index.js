680190c965340ade417f7c4050ea402492b002bb
ariaxhera <velviit17@gmail.com>
2022-08-03 19:42:50 +0200

    day 4

--- solutions/day-03/day3/src/index.js
+++ solutions/day-03/day3/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import asabenehImage from './images/asabenehImage.jpg'
 import frontend from './images/frontend_technologies.png'
 
-const rootElement = document.getElementById('root')
+
 
 const welcome = 'Welcome to 30 Days Of React'
 const title = 'Getting Started React'
@@ -60,120 +60,254 @@ const date = 'Oct 2, 2020'
 
 // EXERCISES ///////////////////////////////////////////////////////////////////////////////////
 
-const Header = () => ( 
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div>
-  </header>
-  )
+const showDate = (time) => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
 
-const numOne = 3
-const numTwo = 2
+  const month = months[time.getMonth()].slice(0, 3)
+  const year = time.getFullYear()
+  const date = time.getDate()
+  return ` ${month} ${date}, ${year}`
+}
 
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
-
-
-const yearBorn = 1820
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
-
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
-
-const UserCard = () => (
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
   <div className='user-card'>
-    <img src={asabenehImage} alt='asabeneh image' />
+    <img src={image} alt={firstName} />
     <h2>
-      {author.firstName} {author.lastName}
+      {firstName}
+      {lastName}
     </h2>
   </div>
 )
 
-// JSX element, main
-const Main = () => (
-  <main>
-    <div className='main-wrapper'>
-      <p>
-        Prerequisite to get started{' '}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
-    </div>
-  </main>
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
 )
 
-const copyRight = 'Copyright 2020'
-
+// CSS styles in JavaScript Object
 const buttonStyles = {
-  padding: '10px 20px',
-  background: 'rgb(0, 255, 0',
+  backgroundColor: '#61dbfb',
+  padding: 10,
   border: 'none',
   borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
 }
-const Button = () => <button style={buttonStyles}> action </button>
 
-const hexaColor = () => {
-  let str = '0123456789avcdef'
-  let color = ''
-  for (let i = 0; i < 6; i++) {
-    let index = Math.floor(Math.random() * str.length)
-    color += str[index]
+// class based component
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    // the code inside the constructor run before any other code
   }
-  return '#' + color
+  render() {
+    console.log(this.props.data)
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
+    return (
+      <header style={this.props.styles}>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    )
+  }
 }
 
-const HexaColor = () => <div>{hexaColor()}</div>
-
-// JSX element, footer
-const Footer = () => (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>{copyRight}</p>
+const Count = ({ count, addOne, minusOne }) => (
+  <div>
+    <h1>{count} </h1>
+    <div>
+      <Button text='+1' onClick={addOne} style={buttonStyles} />
+      <Button text='-1' onClick={minusOne} style={buttonStyles} />
     </div>
-  </footer>
-)
-
-
-
-// JSX element, app
-const App = () => (
-  <div className='app'>
-    <Header />
-    <Main />
-    <UserCard />
-    <Footer />
-    <Button />
-    <HexaColor />
-    {/*/////////////////////////////////////EXERCISES//////////////////////////////////*/}
-    {/* <FrImage />
-    <Exercise2 />
-    <RandomColors /> 
-    {exercise1}
-    {exercise2} */}
   </div>
 )
 
-ReactDOM.render( <App />, rootElement)
+// TechList Component
+// class base component
+class TechList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { techs } = this.props
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+// Main Component
+// Class Component
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const {
+      techs,
+      user,
+      greetPeople,
+      handleTime,
+      changeBackground,
+      count,
+      addOne,
+      minusOne,
+    } = this.props
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList techs={techs} />
+          </ul>
+          <UserCard user={user} />
+          <Button
+            text='Greet People'
+            onClick={greetPeople}
+            style={buttonStyles}
+          />
+          <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
+          <Button
+            text='Change Background'
+            onClick={changeBackground}
+            style={buttonStyles}
+          />
+          <Count count={count} addOne={addOne} minusOne={minusOne} />
+        </div>
+      </main>
+    )
+  }
+}
+
+// Footer Component
+// Class component
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    count: 0,
+    styles: {
+      backgroundColor: '',
+      color: '',
+    },
+  }
+  showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
+  }
+  addOne = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  // method which subtract one to the state
+  minusOne = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+  handleTime = () => {
+    alert(this.showDate(new Date()))
+  }
+  greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+  changeBackground = () => {}
+  render() {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 7, 2020',
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+    const date = new Date()
+    // copying the author from data object to user variable using spread operator
+    const user = { ...data.author, image: asabenehImage }
+
+    return (
+      <div className='app'>
+        {this.state.backgroundColor}
+        <Header data={data} />
+        <Main
+          user={user}
+          techs={techs}
+          handleTime={this.handleTime}
+          greetPeople={this.greetPeople}
+          changeBackground={this.changeBackground}
+          addOne={this.addOne}
+          minusOne={this.minusOne}
+          count={this.state.count}
+        />
+        <Footer date={new Date()} />
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
+
+
